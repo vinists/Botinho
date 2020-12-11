50f200b66f6d2e7fb0c95a2975be440081beac2b
Vinicius <viniciussts@protonmail.com>
2020-12-11 12:41:18 -0300

    added way to test locally with a file instead of having to declare an environment variable everytime.

--- Botinho/Core.py
+++ Botinho/Core.py
@@ -4,9 +4,17 @@ import discord
 import logging
 import os
 
+# To run locally you need to install "python-decouple" with pip and create a .env file with the token.
+try:
+    from decouple import config
+    TOKEN = config("token")
+except ImportError:
+    TOKEN = os.environ["token"]
+
+
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = os.environ["token"]
+
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
